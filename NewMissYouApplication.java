687f2ee8d1f1ada9d6f35cdef29f90995f380683
徐博 <97286151+xuxubo@users.noreply.github.com>
2024-03-11 14:26:59 +0800

    Add files via upload

---
+++ NewMissYouApplication.java
@@ -0,0 +1,13 @@
+package com.example.newmissyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMissYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewMissYouApplication.class, args);
+    }
+
+}
